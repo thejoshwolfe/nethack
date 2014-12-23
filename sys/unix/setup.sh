789734e85ed2051ad14a7edfcca744b2c9407090
Andrew Kelley <superjoe30@gmail.com>
2014-12-22 22:10:55 -0700

    clean up build / run scripts

--- sys/unix/setup.sh
+++ sys/unix/setup.sh
@@ -7,6 +7,5 @@ echo "Copying Makefiles."
 
 cp Makefile.top ../../Makefile
 cp Makefile.dat ../../dat/Makefile
-cp Makefile.doc ../../doc/Makefile
 cp Makefile.src ../../src/Makefile
 cp Makefile.utl ../../util/Makefile
