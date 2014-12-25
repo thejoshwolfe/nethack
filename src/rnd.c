06ec036556229dc0624c389dcb885914758f5040
Josh Wolfe <thejoshwolfe@gmail.com>
2014-12-25 06:41:19 -0700

    cleanup rnd.c

--- src/rnd.c
+++ src/rnd.c
@@ -4,133 +4,56 @@
 /* "Rand()"s definition is determined by [OS]conf.h */
 #define RND(x)  (int)(Rand() % (long)(x))
 
-#ifdef OVL0
-
-int 
-rn2 (           /* 0 <= rn2(x) < x */
-    int x
-)
-{
-#ifdef DEBUG
-        if (x <= 0) {
-                impossible("rn2(%d) attempted", x);
-                return(0);
-        }
-        x = RND(x);
-        return(x);
-#else
-        return(RND(x));
-#endif
+/* 0 <= rn2(x) < x */
+int rn2(int x) {
+  return RND(x);
 }
 
-#endif /* OVL0 */
-#ifdef OVLB
+/* 0 <= rnl(x) < x; sometimes subtracting Luck */
+/* good luck approaches 0, bad luck approaches (x-1) */
+int rnl(int x) {
+  int i;
 
-int 
-rnl (           /* 0 <= rnl(x) < x; sometimes subtracting Luck */
-    int x       /* good luck approaches 0, bad luck approaches (x-1) */
-)
-{
-        int i;
+  i = RND(x);
 
-#ifdef DEBUG
-        if (x <= 0) {
-                impossible("rnl(%d) attempted", x);
-                return(0);
-        }
-#endif
-        i = RND(x);
+  if (Luck && rn2(50 - Luck)) {
+    i -= (x <= 15 && Luck >= -5 ? Luck/3 : Luck);
+    if (i < 0) i = 0;
+    else if (i >= x) i = x-1;
+  }
 
-        if (Luck && rn2(50 - Luck)) {
-            i -= (x <= 15 && Luck >= -5 ? Luck/3 : Luck);
-            if (i < 0) i = 0;
-            else if (i >= x) i = x-1;
-        }
-
-        return i;
+  return i;
 }
 
-#endif /* OVLB */
-#ifdef OVL0
-
-int 
-rnd (           /* 1 <= rnd(x) <= x */
-    int x
-)
-{
-#ifdef DEBUG
-        if (x <= 0) {
-                impossible("rnd(%d) attempted", x);
-                return(1);
-        }
-        x = RND(x)+1;
-        return(x);
-#else
-        return(RND(x)+1);
-#endif
+/* 1 <= rnd(x) <= x */
+int rnd(int x) {
+  return(RND(x)+1);
 }
 
-#endif /* OVL0 */
-#ifdef OVL1
-
-int 
-d (             /* n <= d(n,x) <= (n*x) */
-    int n,
-    int x
-)
-{
-        int tmp = n;
-
-#ifdef DEBUG
-        if (x < 0 || n < 0 || (x == 0 && n != 0)) {
-                impossible("d(%d,%d) attempted", n, x);
-                return(1);
-        }
-#endif
-        while(n--) tmp += RND(x);
-        return(tmp); /* Alea iacta est. -- J.C. */
+/* n <= d(n,x) <= (n*x) */
+int d (int n, int x) {
+  int tmp = n;
+  while (n--) tmp += RND(x);
+  return tmp; /* Alea iacta est. -- J.C. */
 }
 
-#endif /* OVL1 */
-#ifdef OVLB
 
-int 
-rne (int x)
-{
-        int tmp, utmp;
+int rne(int x) {
+  int tmp, utmp;
 
-        utmp = (u.ulevel < 15) ? 5 : u.ulevel/3;
-        tmp = 1;
-        while (tmp < utmp && !rn2(x))
-                tmp++;
-        return tmp;
-
-        /* was:
-         *      tmp = 1;
-         *      while(!rn2(x)) tmp++;
-         *      return(min(tmp,(u.ulevel < 15) ? 5 : u.ulevel/3));
-         * which is clearer but less efficient and stands a vanishingly
-         * small chance of overflowing tmp
-         */
+  utmp = (u.ulevel < 15) ? 5 : u.ulevel/3;
+  tmp = 1;
+  while (tmp < utmp && !rn2(x))
+    tmp++;
+  return tmp;
 }
 
-int 
-rnz (int i)
-{
-#ifdef LINT
-        int x = i;
-        int tmp = 1000;
-#else
-        long x = i;
-        long tmp = 1000;
-#endif
-        tmp += rn2(1000);
-        tmp *= rne(4);
-        if (rn2(2)) { x *= tmp; x /= 1000; }
-        else { x *= 1000; x /= tmp; }
-        return((int)x);
+int rnz(int i) {
+  long x = i;
+  long tmp = 1000;
+  tmp += rn2(1000);
+  tmp *= rne(4);
+  if (rn2(2)) { x *= tmp; x /= 1000; }
+  else { x *= 1000; x /= tmp; }
+  return((int)x);
 }
-
-#endif /* OVLB */
-
-/*rnd.c*/
