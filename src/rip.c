0c572cfc8e397ce0df32cc8d82a83ac793ccc112
Andrew Kelley <superjoe30@gmail.com>
2014-12-26 10:57:13 -0700

    clean up rip.c

--- src/rip.c
+++ src/rip.c
@@ -1,18 +1,12 @@
 /* See LICENSE in the root of this project for change info */
+
 #include "hack.h"
 #include "config.h"
 #include "extern.h"
 #include "winprocs.h"
 
-static void center(int, char *);
-
 extern const char * const killed_by_prefix[];   /* from topten.c */
 
-#define TEXT_TOMBSTONE
-
-#ifdef TEXT_TOMBSTONE
-
-#ifndef NH320_DEDICATION
 /* A normal tombstone for end of game display. */
 static const char *rip_txt[] = {
 "                       ----------",
@@ -33,28 +27,6 @@ static const char *rip_txt[] = {
 0
 };
 #define STONE_LINE_CENT 28      /* char[] element of center of stone face */
-#else   /* NH320_DEDICATION */
-/* NetHack 3.2.x displayed a dual tombstone as a tribute to Izchak. */
-static const char *rip_txt[] = {
-"              ----------                      ----------",
-"             /          \\                    /          \\",
-"            /    REST    \\                  /    This    \\",
-"           /      IN      \\                /  release of  \\",
-"          /     PEACE      \\              /   NetHack is   \\",
-"         /                  \\            /   dedicated to   \\",
-"         |                  |            |  the memory of   |",
-"         |                  |            |                  |",
-"         |                  |            |  Izchak Miller   |",
-"         |                  |            |   1935 - 1994    |",
-"         |                  |            |                  |",
-"         |                  |            |     Ascended     |",
-"         |       1001       |            |                  |",
-"      *  |     *  *  *      | *        * |      *  *  *     | *",
-" _____)/\\|\\__//(\\/(/\\)/\\//\\/|_)________)/|\\\\_/_/(\\/(/\\)/\\/\\/|_)____",
-0
-};
-#define STONE_LINE_CENT 19      /* char[] element of center of stone face */
-#endif  /* NH320_DEDICATION */
 #define STONE_LINE_LEN 16       /* # chars that fit on one line
                                  * (note 1 ' ' border)
                                  */
@@ -65,98 +37,87 @@ static const char *rip_txt[] = {
 
 static char **rip;
 
-static void
-center (int line, char *text)
-{
-        char *ip,*op;
-        ip = text;
-        op = &rip[line][STONE_LINE_CENT - ((strlen(text)+1)>>1)];
-        while(*ip) *op++ = *ip++;
+static void center (int line, char *text) {
+    char *ip,*op;
+    ip = text;
+    op = &rip[line][STONE_LINE_CENT - ((strlen(text)+1)>>1)];
+    while(*ip) *op++ = *ip++;
 }
 
 
-void
-genl_outrip(tmpwin, how)
-winid tmpwin;
-int how;
-{
-        char **dp;
-        char *dpx;
-        char buf[BUFSZ];
-        int x;
-        int line;
+void genl_outrip(winid tmpwin, int how) {
+    char **dp;
+    char *dpx;
+    char buf[BUFSZ];
+    int x;
+    int line;
 
-        rip = dp = (char **) alloc(sizeof(rip_txt));
-        for (x = 0; rip_txt[x]; x++) {
-                dp[x] = (char *) alloc((unsigned int)(strlen(rip_txt[x]) + 1));
-                Strcpy(dp[x], rip_txt[x]);
+    rip = dp = (char **) alloc(sizeof(rip_txt));
+    for (x = 0; rip_txt[x]; x++) {
+        dp[x] = (char *) alloc((unsigned int)(strlen(rip_txt[x]) + 1));
+        Strcpy(dp[x], rip_txt[x]);
+    }
+    dp[x] = (char *)0;
+
+    /* Put name on stone */
+    Sprintf(buf, "%s", plname);
+    buf[STONE_LINE_LEN] = 0;
+    center(NAME_LINE, buf);
+
+    /* Put $ on stone */
+    Sprintf(buf, "%ld Au", u.ugold);
+    buf[STONE_LINE_LEN] = 0; /* It could be a *lot* of gold :-) */
+    center(GOLD_LINE, buf);
+
+    /* Put together death description */
+    switch (killer_format) {
+        default: impossible("bad killer format?");
+        case KILLED_BY_AN:
+                 Strcpy(buf, killed_by_prefix[how]);
+                 Strcat(buf, an(killer));
+                 break;
+        case KILLED_BY:
+                 Strcpy(buf, killed_by_prefix[how]);
+                 Strcat(buf, killer);
+                 break;
+        case NO_KILLER_PREFIX:
+                 Strcpy(buf, killer);
+                 break;
+    }
+
+    /* Put death type on stone */
+    for (line=DEATH_LINE, dpx = buf; line<YEAR_LINE; line++) {
+        int i,i0;
+        char tmpchar;
+
+        if ( (i0=strlen(dpx)) > STONE_LINE_LEN) {
+            for(i = STONE_LINE_LEN; ((i0 > STONE_LINE_LEN) && i); i--)
+                if(dpx[i] == ' ') i0 = i;
+            if(!i) i0 = STONE_LINE_LEN;
         }
-        dp[x] = (char *)0;
+        tmpchar = dpx[i0];
+        dpx[i0] = 0;
+        center(line, dpx);
+        if (tmpchar != ' ') {
+            dpx[i0] = tmpchar;
+            dpx= &dpx[i0];
+        } else  dpx= &dpx[i0+1];
+    }
 
-        /* Put name on stone */
-        Sprintf(buf, "%s", plname);
-        buf[STONE_LINE_LEN] = 0;
-        center(NAME_LINE, buf);
+    /* Put year on stone */
+    Sprintf(buf, "%4d", getyear());
+    center(YEAR_LINE, buf);
 
-        /* Put $ on stone */
-        Sprintf(buf, "%ld Au", u.ugold);
-        buf[STONE_LINE_LEN] = 0; /* It could be a *lot* of gold :-) */
-        center(GOLD_LINE, buf);
+    putstr(tmpwin, 0, "");
+    for(; *dp; dp++)
+        putstr(tmpwin, 0, *dp);
 
-        /* Put together death description */
-        switch (killer_format) {
-                default: impossible("bad killer format?");
-                case KILLED_BY_AN:
-                        Strcpy(buf, killed_by_prefix[how]);
-                        Strcat(buf, an(killer));
-                        break;
-                case KILLED_BY:
-                        Strcpy(buf, killed_by_prefix[how]);
-                        Strcat(buf, killer);
-                        break;
-                case NO_KILLER_PREFIX:
-                        Strcpy(buf, killer);
-                        break;
-        }
+    putstr(tmpwin, 0, "");
+    putstr(tmpwin, 0, "");
 
-        /* Put death type on stone */
-        for (line=DEATH_LINE, dpx = buf; line<YEAR_LINE; line++) {
-                int i,i0;
-                char tmpchar;
-
-                if ( (i0=strlen(dpx)) > STONE_LINE_LEN) {
-                                for(i = STONE_LINE_LEN;
-                                    ((i0 > STONE_LINE_LEN) && i); i--)
-                                        if(dpx[i] == ' ') i0 = i;
-                                if(!i) i0 = STONE_LINE_LEN;
-                }
-                tmpchar = dpx[i0];
-                dpx[i0] = 0;
-                center(line, dpx);
-                if (tmpchar != ' ') {
-                        dpx[i0] = tmpchar;
-                        dpx= &dpx[i0];
-                } else  dpx= &dpx[i0+1];
-        }
-
-        /* Put year on stone */
-        Sprintf(buf, "%4d", getyear());
-        center(YEAR_LINE, buf);
-
-        putstr(tmpwin, 0, "");
-        for(; *dp; dp++)
-                putstr(tmpwin, 0, *dp);
-
-        putstr(tmpwin, 0, "");
-        putstr(tmpwin, 0, "");
-
-        for (x = 0; rip_txt[x]; x++) {
-                free((void *)rip[x]);
-        }
-        free((void *)rip);
-        rip = 0;
+    for (x = 0; rip_txt[x]; x++) {
+        free((void *)rip[x]);
+    }
+    free((void *)rip);
+    rip = 0;
 }
-
-#endif  /* TEXT_TOMBSTONE */
-
-/*rip.c*/
